8627d74d6a9d15a05863ae419931f841696bae74
juancollazo5 <juancollazo577@outlook.com>
2024-01-14 06:14:02 +0000

    update
    eliminate print error on null

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -7,19 +7,18 @@
  * Return: the new node
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) 
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-	if (new_node == NULL) 
+	if (new_node == NULL)
 	{
-		printf("Failed to create new node.\n");
 		return (NULL);
 	}
 
-	new_node->value = value;
+	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-
+	new_node->parent = parent;
 	return (new_node);
 }
